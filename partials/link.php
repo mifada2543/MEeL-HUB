7c8d536a1f6be4a30050121becdca5b2eaa69c2a
mifada2543 <minecraft.daffa2501@gmail.com>
2026-06-16 13:52:40 +0700

    Penyesuaian portofolio

--- partials/link.php
+++ partials/link.php
@@ -1,4 +1,4 @@
-    <link rel="icon" type="image/png" href="../assets/MEeL.png">
-    <script src="../assets/js/tailwind.js"></script>
-    <script src="../assets/js/lucide.js"></script>
-    <link rel="manifest" href="../assets/manifest.json">
+<link rel="manifest" href="../assets/manifest.json">
+<link rel="icon" type="image/png" href="../assets/MEeL.png">
+<script src="../assets/js/tailwind.js"></script>
+<script src="../assets/js/lucide.js"></script>
