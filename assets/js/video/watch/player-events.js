68bbfa6e46925617ea8db9692360ce0096aa48e5
mifada2543 <minecraft.daffa2501@gmail.com>
2026-09-11 07:05:15 +0700

    fix player

--- assets/js/video/watch/player-events.js
+++ assets/js/video/watch/player-events.js
@@ -260,7 +260,9 @@ function setupMeelPlayerEvents() {
                 hls.media !== player.media &&
                 (hls.detachMedia(), hls.attachMedia(player.media)),
             hls.loadSource(s),
-            hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            (window._meelSkipManifestHandler &&
+              hls.off(Hls.Events.MANIFEST_PARSED, window._meelSkipManifestHandler),
+            (window._meelSkipManifestHandler = function () {
               var bitrates = hls.levels.map(function (e) { return e.bitrate; });
               var needsRebuild = !1;
               if (bitrates.length > 1) {
@@ -293,16 +295,15 @@ function setupMeelPlayerEvents() {
                 player = new Plyr(videoElement, plyrOptions);
                 setupMeelPlayerEvents();
                 window.appendCustomSettings && setTimeout(window.appendCustomSettings, 0);
-                if (videoElement.paused === !1) {
-                  var playPromise = player.play();
-                  if (void 0 !== playPromise) {
-                    playPromise.catch(function (e) {
-                      console.error("[MEeL] autoplay dicegah:", e);
-                    });
-                  }
+                var playPromise = player.play();
+                if (void 0 !== playPromise) {
+                  playPromise.catch(function (e) {
+                    console.error("[MEeL] autoplay dicegah:", e);
+                  });
                 }
               }
             }),
+            hls.on(Hls.Events.MANIFEST_PARSED, window._meelSkipManifestHandler)),
             videoElement.addEventListener(
               "loadedmetadata",
               function () {
