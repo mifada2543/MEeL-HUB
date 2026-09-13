5f95d7f1d0468cb4554df6836adcfa6a7a823716
mifada2543 <minecraft.daffa2501@gmail.com>
2026-09-13 07:48:17 +0700

    fix section search

--- assets/js/video/watch/player-events.js
+++ assets/js/video/watch/player-events.js
@@ -253,6 +253,10 @@ function setupMeelPlayerEvents() {
             n = r.getElementById(e);
           t && n && (t.innerHTML = n.innerHTML);
         }),
+        (() => {
+          const el = document.getElementById("rec-title-text");
+          if (el) { el.textContent = "Video Lainnya"; el.title = "Video Lainnya"; }
+        })(),
         window.lucide && window.lucide.createIcons(),
         window.htmx && htmx.process(document.body),
         isMiniPlayerActive ||
