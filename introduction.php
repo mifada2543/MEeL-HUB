5ccffefaa21307235f4b36b1aabb38555047056b
mifada2543 <minecraft.daffa2501@gmail.com>
2026-09-17 08:37:35 +0700

    penyesuaian shortcut

--- introduction.php
+++ introduction.php
@@ -194,6 +194,8 @@ include __DIR__ . '/partials/scripts.php';
                             ['C',         'Toggle caption'],
                             ['L',         'Toggle loop'],
                             ['A',         'Toggle auto-next'],
+                            ['N',         'Video berikutnya'],
+                            ['I',         'Mini player'],
                         ];
                         foreach ($shortcuts_video as $s): ?>
                             <div class="shortcut-item">
@@ -302,6 +304,9 @@ include __DIR__ . '/partials/scripts.php';
                             ['↓',         'Volume turun'],
                             ['M',         'Mute / Unmute'],
                             ['L',         'Toggle loop'],
+                            ['E',         'Toggle equalizer'],
+                            ['V',         'Toggle visualizer'],
+                            ['I',         'Mini player'],
                         ];
                         foreach ($shortcuts_music as $s): ?>
                             <div class="shortcut-item">
